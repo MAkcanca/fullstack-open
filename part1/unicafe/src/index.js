0b095065e6297d6cc3bfe2b1374237ab902383b5
Mustafa Akcanca <MAkcanca@users.noreply.github.com>
2021-01-10 23:21:59 +0300

    Unicafe part 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = ({handleClick, text}) => {
+  return <button onClick={handleClick}>{text}</button>
+}
+const ShowStatistic = ({statName, statVal}) => {
+  return <p>{statName} {statVal}</p>
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseValue = (valueToIncrease, setValue) => {
+    return () => setValue(valueToIncrease + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={increaseValue(good, setGood)} text="good"/>
+      <Button handleClick={increaseValue(neutral, setNeutral)} text="neutral"/>
+      <Button handleClick={increaseValue(bad, setBad)} text="bad"/>
+      <h1>statistics</h1>
+      <ShowStatistic statName="good" statVal={good} />
+      <ShowStatistic statName="neutral" statVal={neutral} />
+      <ShowStatistic statName="bad" statVal={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
